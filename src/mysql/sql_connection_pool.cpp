f8b2ef8bef9085348469d1a94d4180b777e84620
Ni3RaN <wzh47539401@gmail.com>
2022-09-11 02:13:18 +0800

    使用list，MySQL连接池

---
+++ src/mysql/sql_connection_pool.cpp
@@ -0,0 +1,136 @@
+//
+// Created by nie on 22-9-10.
+//
+
+#include "sql_connection_pool.h"
+#include "../log/log.h"
+
+
+sql_connection_pool::sql_connection_pool() {
+    m_CurConn = 0;
+    m_FreeConn = 0;
+}
+
+sql_connection_pool* sql_connection_pool::GetInstance() {
+    static sql_connection_pool connPool;
+    return &connPool;
+}
+
+/**
+ * 初始化数据库连接池
+ * @param url 主机地址
+ * @param User 登陆数据库用户名
+ * @param PassWord 登陆数据库密码
+ * @param DataBaseName 使用数据库名
+ * @param Port 数据库端口号
+ * @param MaxConn 最大连接数
+ */
+void sql_connection_pool::init(std::string url, std::string User, std::string PassWord, std::string DataBaseName,
+                               int Port, int MaxConn, int close_log) {
+    m_url = std::move(url);
+    m_Port = Port;
+    m_User = std::move(User);
+    m_PassWord = std::move(PassWord);
+    m_DatabaseName = std::move(DataBaseName);
+    m_MaxConn = MaxConn;
+    m_close_log = close_log;
+
+    for (int i = 0; i < MaxConn; i++) {
+        MYSQL* conn = nullptr;
+        conn = mysql_init(conn);
+        if (conn == nullptr) {
+            LOG_ERROR("MySQL Error");
+            exit(1);
+        }
+        conn = mysql_real_connect(conn, m_url.c_str(), m_User.c_str(),
+                                                                            m_PassWord.c_str(), m_DatabaseName.c_str(),
+                                                                            m_Port, nullptr, 0);
+        if (conn == nullptr) {
+            LOG_ERROR("MySQL Error");
+            exit(1);
+        }
+        connList.push_back(conn);
+        m_FreeConn++;
+    }
+    reserve = sem(m_FreeConn);
+    m_MaxConn = m_FreeConn;
+}
+
+/**
+ * 当有请求时，从数据库连接池中返回一个可用连接，更新使用和空闲连接数
+ * @return 可用连接
+ */
+MYSQL* sql_connection_pool::GetConnection() {
+    MYSQL* conn = nullptr;
+    if (connList.empty()) {
+        return conn;
+    }
+    reserve.wait();
+    m_lock.lock();
+    conn = connList.front();
+    connList.pop_front();
+    m_FreeConn--;
+    m_CurConn++;
+    m_lock.unlock();
+    return conn;
+}
+
+/**
+ * 释放当前使用的连接
+ * @param conn 当前使用的连接
+ * @return 释放成功返回true
+ */
+bool sql_connection_pool::ReleaseConnection(MYSQL* conn) {
+    if (nullptr == conn) {
+        return false;
+    }
+    m_lock.lock();
+    connList.push_back(conn);
+    m_FreeConn++;
+    m_CurConn--;
+    m_lock.unlock();
+    reserve.post();
+    return true;
+}
+
+/**
+ * 销毁数据库连接池
+ */
+void sql_connection_pool::DestroyPool() {
+    m_lock.lock();
+    if (!connList.empty()) {
+        for (auto &conn: connList) {
+            mysql_close(conn);
+        }
+        m_CurConn = 0;
+        m_FreeConn = 0;
+        connList.clear();
+    }
+    m_lock.unlock();
+}
+
+/**
+ * 当前空闲的连接数
+ * @return 空闲的连接数
+ */
+int sql_connection_pool::GetFreeConn() {
+    return this->m_FreeConn;
+}
+
+/**
+ * 销毁数据库连接池
+ */
+sql_connection_pool::~sql_connection_pool() {
+    DestroyPool();
+}
+
+connectionRAII::connectionRAII(MYSQL** SQL, sql_connection_pool* connPool) {
+    *SQL=connPool->GetConnection();
+    conRAII = *SQL;
+    poolRAII = connPool;
+}
+
+connectionRAII::~connectionRAII() {
+    poolRAII->ReleaseConnection(conRAII);
+}
+
---
+++ src/mysql/sql_connection_pool.h
@@ -0,0 +1,62 @@
+//
+// Created by nie on 22-9-10.
+//
+
+#ifndef NEREIDS_SQL_CONNECTION_POOL_H
+#define NEREIDS_SQL_CONNECTION_POOL_H
+
+#include <mysql/mysql.h>
+#include <error.h>
+#include <string>
+#include <list>
+#include <memory>
+#include "../lock/locker.h"
+
+class sql_connection_pool {
+public:
+    MYSQL* GetConnection();//获取数据库连接
+
+    bool ReleaseConnection(MYSQL* conn);//释放连接
+
+    int GetFreeConn();//获取连接
+
+    void DestroyPool();//销毁所有连接
+
+    static sql_connection_pool* GetInstance();//单例模式
+
+    void init(std::string url, std::string User, std::string PassWord, std::string DataBaseName, int Port,
+              int MaxConn, int close_log);//初始化数据库连接池
+
+private:
+    sql_connection_pool();//构造函数
+    ~sql_connection_pool();//析构函数
+
+    int m_MaxConn;//最大连接数
+    int m_CurConn;//当前已使用的连接数
+    int m_FreeConn;//当前空闲的连接数
+    locker m_lock;//互斥锁
+    std::list<MYSQL* > connList;//连接池
+    sem reserve;//信号量
+
+public:
+    std::string m_url;//主机地址
+    int m_Port;//数据库端口号
+    std::string m_User;//登陆数据库用户名
+    std::string m_PassWord;//登陆数据库密码
+    std::string m_DatabaseName;//使用数据库名
+    int m_close_log;//日志开关
+};
+
+class connectionRAII {
+
+public:
+    connectionRAII(MYSQL** SQL, sql_connection_pool* connPool);
+
+    ~connectionRAII();
+
+private:
+    MYSQL* conRAII;
+    sql_connection_pool* poolRAII;
+};
+
+#endif //NEREIDS_SQL_CONNECTION_POOL_H

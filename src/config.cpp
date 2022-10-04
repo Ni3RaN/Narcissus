00eb01361c27786d5512b38716c199b0f5e25e53
Ni3RaN <wzh47539401@gmail.com>
2022-10-04 15:24:47 +0800

    使用config.json，完成项目的配置

--- src/config.cpp
+++ src/config.cpp
@@ -4,6 +4,10 @@
 Config::Config(){
 
     std::ifstream ifile("config.json");
+    if(!ifile.is_open()){
+        std::cout<<"config file open error"<<std::endl;
+        exit(1);
+    }
     Json::CharReaderBuilder ReaderBuilder;
     ReaderBuilder["emitUTF8"] = true;
     Json::Value json;
@@ -15,61 +19,46 @@ Config::Config(){
         exit(0);
     }
 
+    //端口号
     port = json["config"]["port"].asInt();
 
     //日志写入方式，true 同步,false 异步
     log_write = json["config"]["log_write"].asBool();
 
-
+    //触发组合模式，true LT + LT, false LT + ET
     trig_mode = json["config"]["trig_mode"].asBool();
 
+    //listenfd触发模式，true ET, false LT
     listen_trig_mode = json["config"]["listen_trig_mode"].asBool();
 
+    //connfd触发模式，true ET, false LT
     conn_trig_mode = json["config"]["conn_trig_mode"].asBool();
 
+    //优雅关闭链接，true 优雅关闭, false 正常关闭
     opt_linger = json["config"]["opt_linger"].asBool();
 
-    sql_num = json["config"]["sql_num"].asBool();
+    //数据库连接池数量，默认为8
+    sql_num = json["config"]["sql_num"].asInt();
 
-    thread_num = json["config"]["thread_num"].asBool();
+    //线程池内的线程数量，默认为8
+    thread_num = json["config"]["thread_num"].asInt();
 
     //true 关闭日志,false 不关闭日志
     close_log = json["config"]["close_log"].asBool();
 
+    //数据库端口，默认3306
     mysql_port = json["config"]["mysql"]["port"].asInt();
 
+    //数据库url，默认localhost
     mysql_url = json["config"]["mysql"]["url"].asString();
 
+    //数据库用户名
     mysql_username = json["config"]["mysql"]["username"].asString();
 
+    //数据库密码
     mysql_password = json["config"]["mysql"]["password"].asString();
 
-
+    //选择的数据库
     mysql_database = json["config"]["mysql"]["database"].asString();
-
-//    //端口号,默认9000
-//    PORT = 9000;
-//
-//    //日志写入方式，默认同步
-//    LOGWrite = 0;
-//
-//    //触发组合模式,默认listenfd LT + connfd LT
-//    TRIGMode = 0;
-//
-//    //listenfd触发模式，默认LT
-//    LISTENTrigmode = 0;
-//
-//    //connfd触发模式，默认LT
-//    CONNTrigmode = 0;
-//
-//    //优雅关闭链接，默认不使用
-//    OPT_LINGER = 0;
-//
-//    //数据库连接池数量,默认8
-//    sql_num = 8;
-//
-//    //线程池内的线程数量,默认8
-//    thread_num = 8;
-//
 
 }
